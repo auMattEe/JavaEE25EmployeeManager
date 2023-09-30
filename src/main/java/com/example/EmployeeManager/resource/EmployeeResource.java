8984cb1a656129d03739439cbb7b914d74c16d53
auMattEe <matt.eaton85@gmail.com>
2023-09-30 10:41:11 +0300

    SQL info addedd

--- src/main/java/com/example/EmployeeManager/resource/EmployeeResource.java
+++ src/main/java/com/example/EmployeeManager/resource/EmployeeResource.java
@@ -1,4 +1,28 @@
 package com.example.EmployeeManager.resource;
 
+import com.example.EmployeeManager.model.Employee;
+import com.example.EmployeeManager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeResource {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
 }
