fba46d96e7b105ece9267d9d32de30a6d2b4a93e
auMattEe <matt.eaton85@gmail.com>
2023-09-30 10:10:41 +0300

    EmployeeService updated

--- src/main/java/com/example/EmployeeManager/service/EmployeeService.java
+++ src/main/java/com/example/EmployeeManager/service/EmployeeService.java
@@ -1,9 +1,12 @@
 package com.example.EmployeeManager.service;
 
+import com.example.EmployeeManager.model.Employee;
 import com.example.EmployeeManager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,5 +20,10 @@ public class EmployeeService {
     public EmployeeService(EmployeeRepo employeeRepo) {
         this.employeeRepo = employeeRepo;
 
+    }
+
+    //To find all employees
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
     }
 }
